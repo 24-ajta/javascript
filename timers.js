07227326df666412657099da6e48fe0033dbcd33
ANJITHA NAIR <117582345+24-ajta@users.noreply.github.com>
2023-09-16 09:30:30 +0530

    Add files via upload

---
+++ timers.js
@@ -0,0 +1,31 @@
+var i =0;
+var x = setInterval(function(){
+    document.write("*");
+    i++;
+    if(i==10)
+    {
+        clearInterval(0)
+    }
+},1000)
+
+function abc(){
+    setTimeout(()=>{
+        alert("Welcome");
+    },3000)
+}
+abc();
+
+// var arr = [1,2,3,4];
+// function callback(x){
+//     return x+1;
+// }
+// callback();
+
+// var Arr = [2,3,4,5]
+// //  Callback function
+// function myCallback(x){
+//  return x+1;
+// }
+// // Don't border about .map() for now
+// console.log(Arr.map(myCallback));
+// callback and anonymous 
